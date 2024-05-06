6b7032526c580d54a92faf4d21230a2048019374
Bhavin Savajiyani <bhavinsavajiyani9@gmail.com>
2024-05-07 03:06:42 +0530

    - Add ability to add new game items to collection via form - Add new C# model classes (Genres, Game Details) - Add new C# client classes (GamesClient, GenresClient) - Add new Razor component (EditGamesCollection Page) which represents a form mapping game summary data & user input

---
+++ GameStore_Frontend/Models/GameDetails.cs
@@ -0,0 +1,10 @@
+﻿namespace GameStore_Frontend.Models;
+
+public class GameDetails
+{
+    public int ID {get; set;}
+    public required string Name {get; set;}
+    public string? GenreID {get; set;}
+    public decimal Price {get; set;}
+    public DateOnly ReleaseDate {get; set;}
+}
