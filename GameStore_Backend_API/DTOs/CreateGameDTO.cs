1f2fc9dea87ce7b39af33d6a41efe564b1771787
Bhavin Savajiyani <bhavinsavajiyani9@gmail.com>
2024-05-09 23:06:35 +0530

    - Implement invalid input handling via extension methods - Add data annotations to records - Add new Nuget Package to the project (MinimalApis.Extensions) - Segregate endpoints implementation - Update endpoints which now use MapGroup (in order to group common prefix that can be shared amongst different endpoints)

--- GameStore_Backend_API/DTOs/CreateGameDTO.cs
+++ GameStore_Backend_API/DTOs/CreateGameDTO.cs
@@ -1,7 +1,11 @@
-﻿namespace GameStore_Backend_API.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
 
-public record class CreateGameDTO(
-    string Name,
-    string Genre,
-    decimal Price,
-    DateOnly ReleaseDate);
+namespace GameStore_Backend_API.DTOs;
+
+public record class CreateGameDTO
+(
+    [Required][StringLength(50)] string Name,
+    [Required][StringLength(20)] string Genre,
+    [Range(1, 100)]              decimal Price,
+    DateOnly                     ReleaseDate
+);
