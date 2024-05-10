bf0ec696a6c604f889c8be86700e80fd114003dc
Bhavin Savajiyani <bhavinsavajiyani9@gmail.com>
2024-05-10 17:57:32 +0530

    - Add default genre presets to the database via data seeding - Update C# data class (GameStoreContext) which now contains a callback to populate pre-defined genres upon successful execution of migration process - Update POST endpoint which would now create new game & insert it into the database - Intermediate C# class ("SeedGenres") is automatically generated as a result of successful execution of migration process for data seeding

--- GameStore_Backend_API/Data/GameStoreContext.cs
+++ GameStore_Backend_API/Data/GameStoreContext.cs
@@ -7,4 +7,14 @@ public class GameStoreContext(DbContextOptions<GameStoreContext> options) : DbCo
 {
     public DbSet<Game> Games => Set<Game>();
     public DbSet<Genre> Genres => Set<Genre>();
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Genre>().HasData(
+            new{ID = 1, Name = "Action"},
+            new{ID = 2, Name = "RPG"},
+            new{ID = 3, Name = "Sports"},
+            new{ID = 4, Name = "Casual"},
+            new{ID = 5, Name = "Racing"}
+        );
+    }
 }
---
+++ GameStore_Backend_API/Data/Migrations/20240510105804_SeedGenres.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace GameStore_Backend_API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedGenres : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Genres",
+                columns: new[] { "ID", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Action" },
+                    { 2, "RPG" },
+                    { 3, "Sports" },
+                    { 4, "Casual" },
+                    { 5, "Racing" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "ID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "ID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "ID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "ID",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "ID",
+                keyValue: 5);
+        }
+    }
+}
